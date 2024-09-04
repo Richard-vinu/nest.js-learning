deef44d6c93be38b431a0f9f0f7fa15af08208d6
Richard Andrew T <richard.andrew@zysk.tech>
2024-09-04 23:42:36 +0530

    feat: project setup

---
+++ src/models/about-schema.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+
+const AboutSchema = new mongoose.Schema({
+    title:{
+type:String,
+    },
+    subTitle1: {
+        type: String,
+       
+      },
+      subTitle2: {
+        type: String,
+       ,
+      },
+      youTubeCount: {
+        type: String,
+       ,
+      },
+      instagramCount: {
+        type: String,
+       ,
+      },
+      facebookCount: {
+        type: String,
+       ,
+      },
+      twitterCount: {
+        type: String,
+       ,
+      },
+
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('About', AboutSchema);
